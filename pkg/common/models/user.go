b6f3c92444c1239c376d87069097fec8ac8796d9
Araks <lebedevartemiy74@gmail.com>
2025-02-14 15:38:32 +0500

    Исправил небольшую ошибку с типами

--- pkg/common/models/user.go
+++ pkg/common/models/user.go
@@ -9,7 +9,7 @@ type User struct {
 	gorm.Model
 	Name     string         `gorm:"unique" json:"name"`
 	Password string         `json:"password"`
-	Songs    []models.Song  `gorm:"many2many:user_songs;"`
+	Songs    []Song         `gorm:"many2many:user_songs;"`
 	Albums   []models.Album `gorm:"many2many:user_albums;"`
 	Bands    []models.Band  `gorm:"many2many:user_bands;"`
 	Genres   []models.Genre `gorm:"many2many:user_genres;"`
